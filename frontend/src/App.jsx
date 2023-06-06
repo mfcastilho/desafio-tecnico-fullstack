04a82aba880538dc0c55f50e0e3bae1ee12b9acf
mfcastilho <fredericocastilho@hotmail.com>
2023-06-06 03:44:24 -0300

    feat: creating home page structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from "./pages/Home"
+
+function App() {
+
+
+  return (
+     <body>
+          <Home />
+     </body>    
+  )
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import "./Home.css";
+
+function Home(){
+     return(
+          <main className="home">
+               
+
+               <div className="wrapper-elements">
+                    <h1>Home do Teste Técnico da Shooper</h1>
+                    <input type="file" />
+                    <button>Validar</button>
+               </div>
+
+          </main>
+     );
+}
+
+export default Home;
