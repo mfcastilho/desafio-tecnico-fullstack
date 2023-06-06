66e25d51dd056aedd22a073aee1daa9c56069305
mfcastilho <fredericocastilho@hotmail.com>
2023-06-06 04:36:28 -0300

    feat: importing rect-papaparse

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,62 @@
-import "./Home.css";
+import Papa from "papaparse"
+import { useState } from "react";
 
-function Home(){
-     return(
-          <main className="home">
-               
+function Home() {
 
-               <div className="wrapper-elements">
-                    <h1>Home do Teste Técnico da Shooper</h1>
-                    <input type="file" />
-                    <button>Validar</button>
-               </div>
+     const [setData] = useState([]);
+     const [columnArray, setColumn] = useState([]);
+     const [values, setValues] = useState([]);
 
-          </main>
-     );
+  function handleFile(event){
+     Papa.parse(event.target.files[0], {
+          header: true,
+          skipEmptyLines: true,
+          complete: function(result){
+               const columnArray = [];
+               const valuesArray = [];
+
+               result.data.map((d)=>{
+                    columnArray.push(Object.keys(d));
+                    valuesArray.push(Object.keys(d));
+               });
+               setData(result.data);
+               setColumn(columnArray[0]);
+               setValues(valuesArray);
+          }
+     })
+  }
+
+  return (
+
+      <main className="home">
+        <div className="wrapper-elements">
+          <h1>Home do Teste Técnico da Shooper</h1>
+          <input type="file" accept='.csv' name="file" onChange={handleFile} />
+          <button>Validar</button>
+        </div>
+
+        <br />
+
+        <table>
+          <thead>
+               <tr>
+                    {columnArray.map((col, i) => (
+                         <th key={i}>{col}</th>
+                    ))}
+               </tr>
+          </thead>
+          <tbody>
+               {values.map((v, i) => (
+                    <tr key={i}>
+                         {v.map((value, i) => (
+                              <td key={i}>{value}</td>
+                         ))}
+                    </tr>
+               ))}
+          </tbody>
+        </table>
+      </main>
+  );
 }
 
 export default Home;
